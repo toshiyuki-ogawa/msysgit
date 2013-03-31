d4046a12b3e32b27dfcf64a0c36f68fee1a4d6f9
Toshiyuki Ogawa <Toshiyuki.Ogawa.2.71@gmail.com>
2013-04-01 06:46:54 +0900

    working to be enabled configuable for close-socket

--- close-socket.c
+++ close-socket.c
@@ -2,12 +2,89 @@
 #include <time.h>
 #include "socket-utils.h"
 
+const static int DEFAULT_TIME_WAIT = 240;
+
 static int wait_for_time_out(int fd0, int fd1);
 static time_t get_wait_time(int fd0, int fd1);
 
+static int read_reg_time_wait(time_t *time_wait)
+
+static int read_reg_time_wait(time_t *time_wait)
+{
+	HKEY hk;
+	LONG state;
+	int result;
+	hk = NULL;
+	state = RegOpenKeyExW(HKEY_LOCAL_MATCHINE,
+			L"SYSTEM\\CurrentControlSet\\Services\\Tcpip\\"
+			L"Parameters",
+			NULL,
+			KEY_READ,
+			&hk);
+	if (state == ERROR_SUCCESS) {
+		DWORD data_type;
+		DWORD value;
+		DWORD value_size;
+		value_size = sizeof(value);
+		state = RegQueryValueExW(hk, L"TcpTimedWaitDelay", NULL, 
+			&data_type, &value, &value_size);	
+		if (state == ERROR_SUCCESS && data_type == REG_DWORD) {
+			*time_wait = (time_t)value;
+			result = 1;
+		}
+		else {
+			result = 0;
+		}
+		RegCloseKey(hk);
+	} else {
+		result = 0;
+	}
+	return result;
+}
+
+static int read_time_wait_from_registry(time_t *time_wait)
+{
+	int result;
+	result = read_reg_time_wait(time_wait);
+	if (result)
+	{
+		*time_wait /= 2;
+	}
+	return result;
+}
+
+static int read_time_wait_from_config(time_t *time_wait)
+{
+	int result;
+	long value;
+	result = git_parse_long("win.sock.time.wait", &value);
+	if (result) {
+		*time_wait = (time_t)value;
+	}
+	return result;
+}
+
 static time_t get_wait_time(int fd0, int fd1)
 {
-	return 60 * 1;
+
+	int state;
+	long value;
+	time_t result;
+	int i;
+	int (*read_time_wait[])(time_t *) = {
+		read_time_wait_from_config,
+		read_time_wait_from_registry
+	};
+	for (i = 0; i < sizeof(read_time_wait) / sizeof(read_time_wait[0]); 
+		i++) {
+		if (read_time_wait[i](&result)) {
+			break;
+		}
+	}
+	if (i == sizeof(read_time_wait) / sizeof(read_time_wait[0])) {
+		result = (time_t)DEFAULT_TIME_WAIT;
+	}
+	return result;
 }
 static int wait_for_time_out(int fd0, int fd1)
 {
